4233bcd679f1251c53e795ad43affecac4ed741d
me1314 <me123456789>
2017-02-27 14:32:18 +0800

    注意规范和结构

---
+++ src/main/java/com/me/noteshare/service/Impl/serviceImpl.java
@@ -0,0 +1,9 @@
+package com.me.noteshare.service.Impl;
+
+/**
+ * Author Sunny
+ * Date 2017/2/27
+ * Time 14:29
+ */
+public class serviceImpl {
+}
---
+++ src/main/java/com/me/noteshare/test/test.java
@@ -0,0 +1,9 @@
+package com.me.noteshare.test;
+
+/**
+ * Author Sunny
+ * Date 2017/2/27
+ * Time 14:30
+ */
+public class test {
+}
